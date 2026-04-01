06ff1fb81f4656c4460e0a5451cfb299301d6ec4
Rashmil Kukreja <rashmil.kukreja@gmail.com>
2026-04-01 15:40:53 +0530

    trigger workflow

--- modules/eks/alb-iam-policy.tf
+++ modules/eks/alb-iam-policy.tf
@@ -2,12 +2,12 @@ data "http" "alb_policy_json" {
   url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
 }
 
-data "aws_iam_policy" "alb_controller" {
+resource "aws_iam_policy" "alb_controller" {
   name   = "AWSLoadBalancerControllerIAMPolicy"
-  # policy = data.http.alb_policy_json.response_body
+  policy = data.http.alb_policy_json.response_body
 }
 
 resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
   role       = aws_iam_role.alb_controller.name
-  policy_arn = data.aws_iam_policy.alb_controller.arn
+  policy_arn = aws_iam_policy.alb_controller.arn
 }
